31c943d3796d76efe0859056ca11034b63a7c5cd
guansheng <guanshengli@fstop.com.tw>
2026-01-27 18:02:41 +0800

    Initial commit

---
+++ src/test/java/tw/com/tbb/central/CentralApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.tbb.central;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
