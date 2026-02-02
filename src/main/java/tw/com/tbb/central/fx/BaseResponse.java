ab77a6ba658363114049eef5d07095408bee8034
jackchen <jackchen@misystex.com>
2026-02-02 11:03:55 +0800

    #fx-api:新增schema

--- src/main/java/tw/com/tbb/central/fx/BaseResponse.java
+++ src/main/java/tw/com/tbb/central/fx/BaseResponse.java
@@ -5,12 +5,13 @@ import lombok.experimental.SuperBuilder;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.*;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 @Data
 @SuperBuilder // 必須是 SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BaseResponse {
+    @Schema(description = "回應代碼")
     @lombok.Builder.Default
     private String code = "0000"; // 預設成功代碼
 }
